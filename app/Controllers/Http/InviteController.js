b911f557bd08995f6fd1448b51747f636b96a1f5
Ruan Azevedo <ruan.azevedo@gmail.com>
2019-06-21 15:34:46 -0300

    🎉 Initial commit

---
+++ app/Controllers/Http/InviteController.js
@@ -0,0 +1,57 @@
+'use strict'
+
+/**
+ * Resourceful controller for interacting with invites
+ */
+class InviteController {
+  /**
+   * Show a list of all invites.
+   * GET invites
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new invite.
+   * GET invites/create
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new invite.
+   * POST invites
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single invite.
+   * GET invites/:id
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing invite.
+   * GET invites/:id/edit
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update invite details.
+   * PUT or PATCH invites/:id
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a invite with id.
+   * DELETE invites/:id
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = InviteController
